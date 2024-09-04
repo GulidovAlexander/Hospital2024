b216e724be1dcdd3c1f37edc03990ae1c3e3a15d
Spek <spek@spek.spek>
2024-09-04 16:38:08 +0300

    04.09.2024_update_database

--- BlazorClient/Program.cs
+++ BlazorClient/Program.cs
@@ -1,16 +1,35 @@
+using BlazorClient.Models.Entity;
+
+using Microsoft.EntityFrameworkCore;
+
 namespace BlazorClient
 {
     public class Program
     {
+        public static ApplicationDBContext DBContext { get; private set; }
+
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            var conecctionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            builder.Services.AddDbContext<ApplicationDBContext>(options =>
+                options.UseSqlServer(conecctionString)
+            );
+
+
+
 
             // Add services to the container.
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
 
             var app = builder.Build();
+
+            var serviceScope = app.Services.CreateScope();
+            var serviceProvider = serviceScope.ServiceProvider;
+            DBContext = serviceProvider.GetRequiredService<ApplicationDBContext>();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
