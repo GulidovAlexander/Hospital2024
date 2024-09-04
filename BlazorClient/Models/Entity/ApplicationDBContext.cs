c9f5f94af09c0bec5615b5bd446cc4d26a21feae
Spek <spek@spek.spek>
2024-09-04 16:09:12 +0300

    04.09.2024_add_setting_db

---
+++ BlazorClient/Models/Entity/ApplicationDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorClient.Models.Entity
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .Property(u => u.UserId)
+                .ValueGeneratedOnAdd();
+        }
+    }
+}
---
+++ BlazorClient/Models/Entity/User.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorClient.Models.Entity
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
